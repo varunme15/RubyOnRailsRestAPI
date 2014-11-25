0163f02927d4f53c6917280e453eada815136a7b
Varun Dixit <varun.me15@gmail.com>
2014-11-25 06:33:52 -0800

    RestAPIBaseCode

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyApi::Application.config.secret_key_base = '2879f38b4e7d0080870c441f0b35a8d1a9a77b038321a174cf6aee8b05a4ad87d34947711a569157fee76b5ebce3b0449d9f759947f1722af50c893db1f3e967'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyApi::Application.config.session_store :cookie_store, key: '_myApi_session'
